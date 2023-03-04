ed6fc6961df7d4b20853a6459bc126bd2aa55996
Ankit Maharjan <ankitmaharjan28@gmail.com>
2023-03-04 11:37:55 +0545

    first committ

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+//all the components are load in this file
+import Login from './components/login';
+
+function App() {
+  return (
+    <div style={{marginTop:"55px"}}>
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
